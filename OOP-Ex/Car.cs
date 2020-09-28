95c97cd8cff6b21d4648c72afeba056d7938e9cc
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-09-28 12:12:47 +0200

    made some cars

--- OOP-Ex/Car.cs
+++ OOP-Ex/Car.cs
@@ -6,41 +6,55 @@ namespace OOP_Ex
 {
     class Car
     {
+        //static
+        private static int carCounter = 0;
+        public static int Counter
+        {
+            get { return carCounter; }
+        }
+
+
         //fields
+        int id;
         int modelYear;
         string brand;
         string modelName;
         public string color; // not following encapsulation
 
         string regPlate;
-        public string Regplate 
+        public string RegPlate
         {
-            get
-            {
-                return regPlate;
-            }
-            set
-            {
-                regPlate = value.ToUpper();
-            } 
+            get { return regPlate; }
+            set { regPlate = value.ToUpper(); }
         }
 
 
         //constructor
         public Car(int modelYear, string carBrand, string carModelName)
         {
+            id = ++carCounter;
+
             this.modelYear = modelYear; // this. = if you use the same name in constructor
             brand = carBrand;
             modelName = carModelName;
         }
 
+        public Car(int modelYear, string carBrand, string carModelName, string color, string regPlate)
+            : this(modelYear, carBrand, carModelName) //chain constructor
+        {
+            this.color = color;
+            RegPlate = regPlate;
+        }
+
         public string CarInfo()
         {
-            return $"Car\nBrand: {brand}" +
+            return $"CAR" +
+                $"\nId: {id}" +
+                $"\nBrand: {brand}" +
                 $"\nModel: {modelName}" +
                 $"\nProduction Year: {modelYear}" +
                 $"\nRegistration Plate: {regPlate}" +
-                $"\nColor: {color}";
+                $"\nColor: {color}\n";
         }
     }
 }
--- OOP-Ex/Program.cs
+++ OOP-Ex/Program.cs
@@ -8,13 +8,27 @@ namespace OOP_Ex
     {
         static void Main(string[] args)
         {
-            Car myCar = new Car(2000, "Audi", "RS4");
+            Car myCar = new Car(2000, "Audi", "RS4"); //new instance
 
             myCar.color = "Sky Blue";
 
-            myCar.Regplate = "klm 645";
-            
+            myCar.RegPlate = "klm 645";
+
+            Console.WriteLine("Amount of cars:" + Car.Counter);//call Counter via class
             Console.WriteLine(myCar.CarInfo());
+
+            Car odins = new Car(2009, "Honda", "Civic Type R");
+
+            odins.color = "Racing Red";
+            odins.RegPlate = "hgb 899";
+
+            Console.WriteLine("Amount of cars:" + Car.Counter);//call Counter via class
+            Console.WriteLine(odins.CarInfo());
+
+            Car raisa = new Car(2006, "Volvo", "S80", "Stone Gray", "TlN 776");
+
+            Console.WriteLine("Amount of cars:" + Car.Counter);
+            Console.WriteLine(raisa.CarInfo());
         }
     }
 }
