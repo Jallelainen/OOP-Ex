997b90d7db70fe5f2c3842b0ecc190bfa8da5256
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-09-28 11:30:02 +0200

    Add project files.

---
+++ OOP-Ex/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Ex
+{
+    class Car
+    {
+        //fields
+        int modelYear;
+        string brand;
+        string modelName;
+        public string color; // not following encapsulation
+
+        string regPlate;
+        public string Regplate 
+        {
+            get
+            {
+                return regPlate;
+            }
+            set
+            {
+                regPlate = value.ToUpper();
+            } 
+        }
+
+
+        //constructor
+        public Car(int modelYear, string carBrand, string carModelName)
+        {
+            this.modelYear = modelYear; // this. = if you use the same name in constructor
+            brand = carBrand;
+            modelName = carModelName;
+        }
+
+        public string CarInfo()
+        {
+            return $"Car\nBrand: {brand}" +
+                $"\nModel: {modelName}" +
+                $"\nProduction Year: {modelYear}" +
+                $"\nRegistration Plate: {regPlate}" +
+                $"\nColor: {color}";
+        }
+    }
+}
---
+++ OOP-Ex/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP_Ex
+{
+    //deafult access modifier is private.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car(2000, "Audi", "RS4");
+
+            myCar.color = "Sky Blue";
+
+            myCar.Regplate = "klm 645";
+            
+            Console.WriteLine(myCar.CarInfo());
+        }
+    }
+}
